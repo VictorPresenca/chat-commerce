81fe85560951f3005d868b4c00c8ba0e472b33d4
VictorPresenca <presenca.victor@gmail.com>
2025-12-15 22:47:54 -0300

    estrutura básica

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+export default function AdminPage(){
+    return (
+        <main className="p-8">
+            <h1 className="text-2x1 font-bold">
+                Painel Administrativo
+            </h1>
+            <p className="mt-2 text-gray-600">
+                Gerencie produtos e pedidos.
+            </p>
+        </main>
+    );
+}
