858b416c781ba4ee12f4c8bfc4a0bb076d6094e2
VictorPresenca <presenca.victor@gmail.com>
2025-12-30 15:29:26 -0300

    adição do minimo para deploy nas pgtsx order e profile em account

---
+++ src/app/account/orders/page.tsx
@@ -0,0 +1,8 @@
+export default function OrdersPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Meus Pedidos</h1>
+      <p>Em breve você poderá visualizar seu histórico de pedidos aqui.</p>
+    </main>
+  );
+}
---
+++ src/app/account/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function OrdersPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Minha conta</h1>
+      <p>Em breve você poderá visualizar sua conta aqui.</p>
+    </main>
+  );
+}
