9cf46e5f9da6d5cd51cfad81881be1266cbbd36f
VictorPresenca <presenca.victor@gmail.com>
2026-01-04 23:00:01 -0300

    login 3

--- src/app/account/profile/page.tsx
+++ src/app/account/profile/page.tsx
@@ -3,6 +3,7 @@ export default function OrdersPage() {
     <main className="p-6">
       <h1 className="text-2xl font-bold">Minha conta</h1>
       <p>Em breve você poderá visualizar sua conta aqui.</p>
+      <p>E fazer edições de dados pessoais</p>
     </main>
   );
 }
