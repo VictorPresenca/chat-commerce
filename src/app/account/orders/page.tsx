348bc2a4a8f7c661fff82a6ea1a008ddb8a760f6
VictorPresenca <presenca.victor@gmail.com>
2026-01-04 22:44:30 -0300

    alteração para função de login 2

--- src/app/account/orders/page.tsx
+++ src/app/account/orders/page.tsx
@@ -3,6 +3,7 @@ export default function OrdersPage() {
     <main className="p-6">
       <h1 className="text-2xl font-bold">Meus Pedidos</h1>
       <p>Em breve você poderá visualizar seu histórico de pedidos aqui.</p>
+      <p>E fazer algumas funções com seus pedidos também!</p>
     </main>
   );
 }
